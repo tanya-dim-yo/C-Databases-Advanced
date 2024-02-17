1d21650347cb95253c2d12fc3857fa1782cbe077
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-02-18 00:14:05 +0200

    Add files via upload

---
+++ Entity Framework Introduction/02. Database First/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            var empl1 = context.Employees.Find(1);
+
+            Console.WriteLine(empl1.FirstName);
+        }
+    }
+}
