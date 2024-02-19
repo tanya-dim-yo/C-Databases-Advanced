d64101ae8678196a25a79eaec654d3a0f1a38c72
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-02-19 21:01:27 +0200

    Add files via upload

---
+++ Entity Framework Introduction/03. Employees Full Information/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                }).ToList();
+
+            string result = string.Join(Environment.NewLine,
+                employees.Select(e => $"{ e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}"));
+
+            return result;
+        }
+    }
+}
