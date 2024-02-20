5c62dc3ea51e2ee2870182e4abe89899de5894d7
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-02-20 21:33:14 +0200

    Add files via upload

---
+++ Entity Framework Introduction/04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                }).ToList();
+
+            string result = string.Join(Environment.NewLine,
+                employees.Select(e => $"{ e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}"));
+
+            return result;
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine,
+                employees.Select(e => $"{e.FirstName} - {e.Salary:F2}"));
+
+            return result;
+        }
+    }
+}
