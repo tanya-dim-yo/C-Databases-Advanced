0dd44d497f6c03f6956ccf187f7d84375660b085
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-02-22 18:33:35 +0200

    Add files via upload

---
+++ Entity Framework Introduction/05. Employees from Research and Development/Models/EmployeeProject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni.Models
+{
+    public class EmployeeProject
+    {
+        public int EmployeeID { get; set; }
+        public virtual Employee Employee { get; set; }
+        public int ProjectID { get; set; }
+        public virtual Project Project { get; set;}
+    }
+}
