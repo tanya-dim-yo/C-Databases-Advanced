5f145412b2bb3e95cc4ff2b9d25b1c4b9c6b3802
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-02-25 01:52:05 +0200

    Add files via upload

---
+++ Entity Framework Introduction/06. Adding a New Address and Updating Employee/StartUp.cs
@@ -0,0 +1,101 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(AddNewAddressToEmployee(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                }).ToList();
+
+            string result = string.Join(Environment.NewLine,
+                employees.Select(e => $"{ e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}"));
+
+            return result;
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine,
+                employees.Select(e => $"{e.FirstName} - {e.Salary:F2}"));
+
+            return result;
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var rndEmployees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName);
+
+            string result = string.Join(Environment.NewLine,
+                rndEmployees.Select(e => $"{e.FirstName} {e.LastName} from {e.Name} - ${e.Salary:F2}"));
+
+            return result;
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            Address address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            var employee = context.Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+
+            employee.Address = address;
+
+            context.SaveChanges();
+
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.AddressId,
+                    e.Address.AddressText
+                })
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .ToList();
+
+            string result = string.Join(Environment.NewLine, employees.Select(e => $"{e.AddressText}"));
+
+            return result;
+        }
+    }
+}
